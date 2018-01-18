7b1a1e6a1bb9b1059ca2d29cb247a9227061add0
blackher <ashitudou@126.com>
2018-01-18 17:25:36 +0800

    update .gitignore

--- config/config.local.js
+++ config/config.local.js
@@ -8,9 +8,9 @@ exports.mysql = {
     // 用户名
     user: 'root',
     // 密码
-    password: '123456',
+    password: 'Zd!=Shihe1990',
     // 数据库名
-    database: 'egg-test',
+    database: 'api',
   },
   // 是否加载到 app 上，默认开启
   app: true,
@@ -19,7 +19,7 @@ exports.mysql = {
 };
 exports.redis = {
   client: {
-    host: '192.168.11.98',
+    host: '127.0.0.1',
     port: '6379',
     password: '',
     db: '0',
@@ -28,8 +28,8 @@ exports.redis = {
 };
 //小程序appId  key
 exports.weappSDK = {
-    appId: '',
-    appSecret: '',
+    appId: 'wxc8cf72438924f1bc',
+    appSecret: 'dc2920d924e05471e47e266f231e9365',
 };
 //设置session
 exports.sessionRedis = {
@@ -40,10 +40,10 @@ exports.sessionRedis = {
 
 exports.sequelize = {
   dialect: 'mysql', // support: mysql, mariadb, postgres, mssql
-  database: 'egg-test',
+  database: 'api',
   host: '127.0.0.1',
   port: '3306',
   username: 'root',
-  password: '123456',
+  password: 'Zd!=Shihe1990',
   operatorsAliases:false,
 };
