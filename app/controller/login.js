5ea5ee4dcbb61501ad9778d3b13099e8cbeeea79
shihe@wanglibank.com <shihe@wanglibank.com>
2018-01-19 13:50:05 +0800

    update

--- app/controller/login.js
+++ app/controller/login.js
@@ -13,42 +13,49 @@ class loginController extends Controller {
   async index() {
     const ctx = this.ctx;
     //没有登陆的情况下
-    if(ctx.session === undefined || JSON.stringify(ctx.session) == "{}" ){
-    	//校验是否带有参数
-    	ctx.validate(createRule);
+    try{
+        if(ctx.session === undefined || JSON.stringify(ctx.session) == "{}" ){
+        	//校验是否带有参数
+        	ctx.validate(createRule);
 
-    	const code = ctx.request.body.code;
-    	
-    	//登陆获取用的openid sessionid
+        	const code = ctx.request.body.code;
+        	
+        	//登陆获取用的openid sessionid
 
-    	const res = await ctx.service.jscode2session.getSessionKey(code);
-        try{
-        	const result = await  ctx.model.User.findOrCreate({
-        		where:{
-        			'openid':res.openid,
+        	const res = await ctx.service.jscode2session.getSessionKey(code);
+            
+            	const result = await  ctx.model.User.findOrCreate({
+            		where:{
+            			'openid':res.openid,
 
-        		},
-        		defaults:{
-        			'openid':res.openid,
-        		}
+            		},
+            		defaults:{
+            			'openid':res.openid,
+            		}
 
-        	})
-            const userInfo = await ctx.model.User.find({
-                where:{
-                    'openid':res.openid
-                }
-            })
-        }catch(err){
+            	})
+                const userInfo = await ctx.model.User.find({
+                    where:{
+                        'openid':res.openid
+                    }
+                })
+            }catch(err){
+
+                this.ctx.throw(401, err);
+            }
+        	//记录到session
+        	ctx.session.user = JSON.stringify(userInfo);
+        }
+        ctx.body = {
+          message:ctx.session.user
+        };
+        ctx.status = 201;
+        }
+
+    }catch(err){
 
             this.ctx.throw(401, err);
-        }
-    	//记录到session
-    	ctx.session.user = JSON.stringify(userInfo);
     }
-    ctx.body = {
-      message:ctx.session.user
-    };
-    ctx.status = 201;
   }
 }
 
